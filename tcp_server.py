16c482ad866041ab4b4bf5ecf14d46f6b46ca55b
ravish1729 <ravish1729@gmail.com>
2018-05-02 23:04:39 +0530

    some improvement

--- tcp_server.py
+++ tcp_server.py
@@ -22,8 +22,7 @@ def index_peers(files_data,address):
 	return l
     
 # Code begins here
-peers=[]              # stores list of peers
-all_files=list()
+all_files=list()  # stores list of peers with file details
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 host= inet_addr()
@@ -32,32 +31,33 @@ s.bind((host,port))
 
 print("waiting..."+str(host))
 s.listen(5)
+flag=0
 while True:
 	conn,addr=s.accept()
 	print("connected "+str(addr))
-	peers.append(addr)
-	print(peers)
 	
 	conn.sendall(b"Index Server wants File list")
 	files_data=conn.recv(1024).decode()
-	all_files.append(index_peers(files_data,addr[0]))
+	received_files=index_peers(files_data,addr[0])
+	ip=received_files[0]
+	upgrade_flag=1
+	if(flag):
+		print(ip,all_files[0][0])
+		for i,j in enumerate(all_files):
+			if(str(all_files[i][0]) == str(ip)):
+				print("True")
+				del all_files[i]
+				all_files.append(received_files)
+				upgrade_flag=0
+				break
+	if(upgrade_flag):
+		all_files.append(received_files)
+	flag=1
 	print(all_files)
 	
-	dataa=pickle.dumps(all_files)
-	conn.sendall(dataa)
+	data=pickle.dumps(all_files)
+	conn.sendall(data)
 	print("Sent data to peer")
 	conn.close()
 s.close()
-
-
-
-
-
-
-
-
-
-
-
-
 
